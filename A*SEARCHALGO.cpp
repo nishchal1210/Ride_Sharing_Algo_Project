3a4d115e9c7ac3f7a83ba14848bd69647771c981
philnumpy <166856319+philnumpy@users.noreply.github.com>
2024-11-04 22:23:55 +0530

    Update A*SEARCHALGO.cpp

--- A*SEARCHALGO.cpp
+++ A*SEARCHALGO.cpp
@@ -1,7 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-int ROW;
-int COL;
 typedef pair<int,int> Pair;
 typedef pair<double,pair<int,int>> pPair;
 //class to define cell and its parameters like coordinates of parents cells and costs
@@ -12,14 +10,17 @@ struct cell{
    double f,g,h;
 };
 class ASTARSEARCHALGORITHM{
-private:
+public:
+   int ROW,COL;
+   ASTARSEARCHALGORITHM(int row,int col):ROW(row),COL(col){}
+
    //function to check that coordinates of cells are valid or not
    bool isvalid(int row,int col){
     if(row>=0&&row<ROW&&col>=0&&col<COL) return true;
     return false;
    }
    //function to check if a given cell is unblocked or not,1-unblocked(true returned),0-blocked(false returned)
-   bool isunblocked(int row,int col,int grid[][COL]){
+   bool isunblocked(int row,int col,const vector<vector<int>>&grid){
     if(grid[row][col]==1) return true;
     else return false;
    }
@@ -35,7 +36,8 @@ private:
         + (col - dest.second) * (col - dest.second)));
    }
    //function to trace path from source cell tpo destination cell
-   void tracepath(struct cell celldetails[][COL],Pair dest){
+   void tracepath(cell celldetails[][100],Pair dest){
+    cout<<"\nthe path is";
     //initialse variables to coordinates of destination cell
     int i=dest.first;
     int j=dest.second;
@@ -43,8 +45,8 @@ private:
     stack<Pair> path;
     //a loop that runs till we reach source cell
     //parent coordinates of source cell are source coordinates itself
-    while(!(celldetails[i][j].parent_i==i&&celldetails.parent_j==j)){
-        stack.push(make_pair(i,j));
+    while(!(celldetails[i][j].parent_i==i&&celldetails[i][j].parent_j==j)){
+        path.push(make_pair(i,j));
         int temp_i=celldetails[i][j].parent_i;
         int temp_j=celldetails[i][j].parent_j;
         i=temp_i;
@@ -57,36 +59,38 @@ private:
         pair<int,int> p=path.top();
         path.pop();
         cout<<"("<<p.first<<","<<p.second<<")";
+        if(!path.empty())cout<<"->";
 
 
     }
+    cout<<endl;
     return;
 
     
    }
    //function to implemet astarsearch to find shortest path
-   void astareach(int grid[][COl],Pair src,Pair dest){
+   void astareach(const vector<vector<int>>&grid,Pair src,Pair dest){
     //to check if source and destination coordinates are valid or not
     if(!isvalid(src.first,src.second)){
-        cout<<"source is invalid";
+        cout<<"source is invalid"<<endl;
         return;
     }
     if(!isvalid(dest.first,dest.second)){
-        cout<<"destination is invalid";
+        cout<<"destination is invalid"<<endl;
         return;
     }
     //to check is source or destination are unblocked
     if(!isunblocked(src.first,src.second,grid)){
-        cout<<"source is blocked";
+        cout<<"source is blocked"<<endl;
         return;
     }
     if(!isunblocked(dest.first,dest.second,grid)){
-        cout<<"destination is blocked";
+        cout<<"destination is blocked"<<endl;
         return;
     }
     //check is source is destination or not
     if(isdestination(src.first,src.second,dest)){
-        cout<<"we already are at the destination";
+        cout<<"we already are at the destination"<<endl;
         return;
 
     }
@@ -95,17 +99,17 @@ private:
     //it is of bool type
     //closed list hepls to avoid cells that have been already evaluated 
     //improves effficiency
-    bool closedlist[ROW][COL]={false};
+    vector<vector<bool>> closedlist(ROW,vector<bool>(COL,false));
     //declaring a 2d array of class cell to hold details of every cell
-    struct cell celldetails[ROW][COL];
+    cell celldetails[100][100];
     int i,j;
     for(i=0;i<ROW;i++){
-        for(j=0;j<COLj++){
+        for(j=0;j<COL;j++){
             celldetails[i][j].parent_i=-1;
             celldetails[i][j].parent_j=-1;
             celldetails[i][j].f=FLT_MAX;
             celldetails[i][j].g=FLT_MAX;
-            celldetails[i][j].g=FLT_MAX;
+            celldetails[i][j].h=FLT_MAX;
 
         }
     }
@@ -130,7 +134,7 @@ private:
         openlist.erase(openlist.begin());
         i=p.second.first;
         j=p.second.second;
-        //cell is evalaution so change its bool value in closed list
+        //cell is under evalaution so change its bool value in closed list
         closedlist[i][j]=true;
         
         //generate 8 successors of this cell
@@ -147,7 +151,7 @@ private:
        //declare f,g,h cost value of successor cells
        double hnew,gnew,fnew;
        //1st Successor-(i,j+1)
-       //only continue is cell is valid one
+       //only continue if cell is valid one
        if(isvalid(i,j+1)){
         //if successor is same as destination
         if(isdestination(i,j+1,dest)){
@@ -171,7 +175,7 @@ private:
             //if successor is in open list
             //check if it is better path to destination
             //compare f value and fnew
-            if(celldetails[i][j+1].f=FLT_MAX||celldetails[i][j+1].f>fnew){
+            if(celldetails[i][j+1].f==FLT_MAX||celldetails[i][j+1].f>fnew){
                 //inserting to open list
                 openlist.insert(make_pair(fnew,make_pair(i,j+1)));
                  //update its details
@@ -203,7 +207,7 @@ private:
             hnew=calculatehvalue(i,j-1,dest);
             fnew=gnew+hnew;
             //if not on openlist or fnew<f
-            if(celldetails[i][j-1].f=FLT_MAX||celldetails[i][j-1].f>fnew){
+            if(celldetails[i][j-1].f==FLT_MAX||celldetails[i][j-1].f>fnew){
                 openlist.insert(make_pair(fnew,make_pair(i,j-1)));
                 celldetails[i][j-1].g=gnew;
                 celldetails[i][j-1].h=hnew;
@@ -217,7 +221,7 @@ private:
        }
        //3rd successor-(i-1,j)
        if(isvalid(i-1,j)){
-        if(isdestination(i-1,j)){
+        if(isdestination(i-1,j,dest)){
             celldetails[i-1][j].parent_i=i;
             celldetails[i-1][j].parent_j=j;
             cout<<"destination is found"<<endl;
@@ -231,7 +235,7 @@ private:
             gnew=celldetails[i][j].g+1;
             hnew=calculatehvalue(i-1,j,dest);
             fnew=gnew+hnew;
-            if(celldetails[i-1][j].f=FLT_MAX||celldetails[i-1][j].f>fnew){
+            if(celldetails[i-1][j].f==FLT_MAX||celldetails[i-1][j].f>fnew){
                 openlist.insert(make_pair(fnew,make_pair(i-1,j)));
                 celldetails[i-1][j].g=gnew;
                 celldetails[i-1][j].h=hnew;
@@ -254,11 +258,11 @@ private:
             found_dest=true;
             return;
         }
-        esle if(closedlist[i+1][j]==false&&isunblocked(i+1,j,grid)){
+        else if(closedlist[i+1][j]==false&&isunblocked(i+1,j,grid)){
             gnew=celldetails[i][j].g+1;
             hnew=calculatehvalue(i+1,j,dest);
             fnew=gnew+hnew;
-            if(celldetails[i+1][j].f=FLT_MAX||celldetails[i+1][j].f>fnew){
+            if(celldetails[i+1][j].f==FLT_MAX||celldetails[i+1][j].f>fnew){
                 openlist.insert(make_pair(fnew,make_pair(i+1,j)));
                 celldetails[i+1][j].g=gnew;
                 celldetails[i+1][j].h=hnew;
@@ -280,11 +284,11 @@ private:
             found_dest=true;
             return;
         }
-        else if(celldetails[i-1][j-1]==false&&isunblocked(i-1,j-1,grid)){
+        else if(closedlist[i-1][j-1]==false&&isunblocked(i-1,j-1,grid)){
             gnew=celldetails[i][j].g+sqrt(2);
             hnew=calculatehvalue(i-1,j-1,dest);
             fnew=gnew+hnew;
-            if(celldetails[i-1][j-1].f=FLT_MAX||celldetails[i-1][j-1].f>fnew){
+            if(celldetails[i-1][j-1].f==FLT_MAX||celldetails[i-1][j-1].f>fnew){
                 openlist.insert(make_pair(fnew,make_pair(i-1,j-1)));
                 celldetails[i-1][j-1].g=gnew;
                 celldetails[i-1][j-1].h=hnew;
@@ -305,11 +309,11 @@ private:
             found_dest=true;
             return;
         }
-        else if(celldetails[i-1][j+1]==false&&isunblocked(i-1,j+1,grid)){
+        else if(closedlist[i-1][j+1]==false&&isunblocked(i-1,j+1,grid)){
             gnew=celldetails[i][j].g+sqrt(2);
             hnew=calculatehvalue(i-1,j+1,dest);
             fnew=gnew+hnew;
-            if(celldetails[i-1][j+1].f=FLT_MAX||celldetails[i-1][j+1].f>fnew){
+            if(celldetails[i-1][j+1].f==FLT_MAX||celldetails[i-1][j+1].f>fnew){
                 openlist.insert(make_pair(fnew,make_pair(i-1,j+1)));
                 celldetails[i-1][j+1].g=gnew;
                 celldetails[i-1][j+1].h=hnew;
@@ -332,11 +336,11 @@ private:
             found_dest=true;
             return;
         }
-        else if(celldetails[i+1][j+1]==false&&isunblocked(i+1,j+1,grid)){
+        else if(closedlist[i+1][j+1]==false&&isunblocked(i+1,j+1,grid)){
             gnew=celldetails[i][j].g+sqrt(2);
             hnew=calculatehvalue(i+1,j+1,dest);
             fnew=gnew+hnew;
-            if(celldetails[i+1][j+1].f=FLT_MAX||celldetails[i+1][j+1].f>fnew){
+            if(celldetails[i+1][j+1].f==FLT_MAX||celldetails[i+1][j+1].f>fnew){
                 openlist.insert(make_pair(fnew,make_pair(i+1,j+1)));
                 celldetails[i+1][j+1].g=gnew;
                 celldetails[i+1][j+1].h=hnew;
@@ -357,11 +361,11 @@ private:
             found_dest=true;
             return;
         }
-        else if(celldetails[i+1][j-1]==false&&isunblocked(i+1,j-1,grid)){
+        else if(closedlist[i+1][j-1]==false&&isunblocked(i+1,j-1,grid)){
             gnew=celldetails[i][j].g+sqrt(2);
             hnew=calculatehvalue(i+1,j-1,dest);
             fnew=gnew+hnew;
-            if(celldetails[i+1][j-1].f=FLT_MAX||celldetails[i+1][j-1].f>fnew){
+            if(celldetails[i+1][j-1].f==FLT_MAX||celldetails[i+1][j-1].f>fnew){
                 openlist.insert(make_pair(fnew,make_pair(i+1,j+1)));
                 celldetails[i+1][j-1].g=gnew;
                 celldetails[i+1][j-1].h=hnew;
@@ -376,13 +380,13 @@ private:
        }
     }
     if(found_dest==false){
-        cout<<"failed to find destination cell";
+        cout<<"failed to find destination cell"<<endl;
         return;
     }
 
    }
-public:
-   void implement_a_star_search(int grid[][COL],Pair src,Pair dest){
+
+   void implement_a_star_search(vector<vector<int>>grid,Pair src,Pair dest){
     astareach(grid,src,dest);
     return;
    }
@@ -394,10 +398,11 @@ public:
 
 };
 
-int main{
+int main(){
+    int ROW,COL;
     cin>>ROW;
     cin>>COL;
-    int grid[ROW][COL];
+    vector<vector<int>>grid(ROW,vector<int>(COL));
     //1-cell unblocked
     //0-cell blocked
     for(int i=0;i<ROW;i++){
@@ -411,8 +416,10 @@ int main{
     cin>>dest_j;
     Pair src=make_pair(source_i,source_j);// pair containing coordinates
     Pair dest=make_pair(dest_i,dest_j);
-    ASTARSEARCHALGORITHM X;
+    ASTARSEARCHALGORITHM X(ROW,COL);
+
     X.implement_a_star_search(grid,src,dest);
+    return 0;
     
 
 
